42bc677092cd5b4bf48e38a59d1742496220b729
trantuhao <tuhao99@gmail.com>
2020-12-07 01:13:50 +0700

    block non-admin users from login

--- component/auth/authController.js
+++ component/auth/authController.js
@@ -7,24 +7,24 @@ module.exports = {
         let {email, password} = req.body;
 
         const user = await User.find({email});
-        if(user.length === 0)
-            res.status(400).send({message:  "email does not exist"})
-        else {
-            // const result = bcrypt.compareSync(password, user[0].password);
-            const result = password === user[0].password;
-            if (result) {
-                console.log(user[0]);
-                const token = jwt.sign({_id: user[0]._id}, process.env.ACCESS_TOKEN_SECRET);
-                res.status(200).send({
-                    token,
-                    user: user[0]
-                })
-            } else {
-                res.status(400).send({message:  "password wrong"})
-            }
+        if (user.length === 0)
+            return res.status(400).send({message: "email does not exist"})
+
+        // const result = bcrypt.compareSync(password, user[0].password);
+        const result = password === user[0].password;
+        if (!result) {
+            return res.status(400).send({message: "password wrong"})
         }
 
+        if(!user[0].isAdmin) {
+            return res.status(400).send({message: "you dont have permission to login this page"})
+        }
 
-
+        // console.log(user[0]);
+        const token = jwt.sign({_id: user[0]._id}, process.env.ACCESS_TOKEN_SECRET);
+        return res.status(200).send({
+            token,
+            user: user[0]
+        })
     }
 }
