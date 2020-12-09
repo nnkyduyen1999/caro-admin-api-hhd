179bf1e5d27cfb339fced35365ed59f05731855d
trantuhao <tuhao99@gmail.com>
2020-12-09 23:00:12 +0700

    fix heroku

--- component/auth/authApi.js
+++ component/auth/authApi.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authController = require('./authController');
 
-
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
 router.post('/login', authController.login);
 
 module.exports = router;
