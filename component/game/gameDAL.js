7e7d93394e3d57ae7c411e646f3487e5f125e121
1712397-KD <1712397@student.hcmus.edu.vn>
2021-01-14 00:51:26 +0700

    get list finihed game

--- component/game/gameDAL.js
+++ component/game/gameDAL.js
@@ -3,7 +3,11 @@ const Game = require("./gameModel");
 module.exports = {
   allFinishGame: () => {
       return Game.find({isFinish: true});
-  }
+  },
 
-  
+  getFinishedGamesByUserIdDAL: (userId) => {
+    return Game.find()
+      .or([{ xPlayer: userId }, { oPlayer: userId }])
+      .and([{ isFinish: true }]);
+  },
 };
