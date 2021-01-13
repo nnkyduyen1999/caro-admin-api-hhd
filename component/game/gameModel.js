3c0d007817bd56b9f1dca0aa2c8784a144cdf09e
Hang Thanh <hangthanh180599@gmail.com>
2021-01-13 16:41:07 +0700

    add game model

---
+++ component/game/gameModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const gameSchema = new Schema({
+    xPlayer: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    oPlayer: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    roomId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Room',
+        required: true
+    },
+    createTime: {
+        type: Date,
+        default: Date.now
+    },
+    history: {
+        type: []
+    },
+    winningLine: {
+        type: []
+    },
+    winner: {
+        type: String,
+    },
+    isFinish: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model("Game", gameSchema);
